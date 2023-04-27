9035e98fc8728b78bd23b66313d4da201b217b22
Habtamu Asmare <habtasm@dtu.edu.et>
2023-04-27 13:58:21 +0300

    added test for fizzbuzz

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -44,3 +44,15 @@ describe Solver do
     end
   end
 
+  describe '#fizzbuzz' do
+    context 'when given an integer' do
+      it 'returns the correct string' do
+        expect(@solver.fizzbuzz(3)).to eq('fizz')
+        expect(@solver.fizzbuzz(5)).to eq('buzz')
+        expect(@solver.fizzbuzz(15)).to eq('fizzbuzz')
+        expect(@solver.fizzbuzz(7)).to eq('7')
+      end
+    end
+  end
+end
+
